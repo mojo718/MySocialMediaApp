1766f382a6f91898eff796e5594e079610405a96
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-05-22 20:48:09 -0400

    Modified Readme file

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# MySocialMediaApp
+
+# Project Title
+MySocialMediaBackend
+
+![Static Badge](https://img.shields.io/badge/MIT-blue.svg) 
+
+
+## Description 🔎
+Social Media Backend
+
+## Table of Contents 📖
+- [Installation](#installation-⚙️)
+- [Usage](#usage-🎮)
+- [License](#license-MIT)
+- [Contibuters](#Contributors-🧑‍🤝‍🧑)
+- [Deployed Application](#deployed-application-🚀)
+- [Tuturial Link](#tuturial-link)
+
+## Installation ⚙️
+instructions
+
+## Usage 🎮
+NPM Run seed then NPM Start
+
+## License
+      This application is covered by the MIT license.
+
+## Contributors 🧑‍🤝‍🧑
+Ben M
+
+## Tuturial Link
+
+
+## Questions 🙋
+If there are any questions, feel free to contact my email at: mojo718@yahoo.com
+
+You can also find me on GitHub at: [mojo718](https://www.github.com/mojo718)
--- models/Thoughts.js
+++ models/Thoughts.js
@@ -2,18 +2,44 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const reactionSchema = new Schema({
-    reactionId: { type: Schema.Types.ObjectId, default: () => new mongoose.Types.ObjectId() },
-    reactionBody: { type: String, required: true, maxlength: 280 },
-    username: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, get: createdAtVal => createdAtVal.toISOString() }
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => createdAtVal.toISOString()
+    }
 }, {
     toJSON: { getters: true }
 });
 
 const thoughtSchema = new Schema({
-    thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
-    createdAt: { type: Date, default: Date.now, get: createdAtVal => createdAtVal.toISOString() },
-    username: { type: String, required: true },
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => createdAtVal.toISOString()
+    },
+    username: {
+        type: String,
+        required: true
+    },
     reactions: [reactionSchema]
 }, {
     toJSON: { virtuals: true, getters: true },
